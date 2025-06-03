664bb5d4ff5e680e65c4f774d8af97e1f89b19e2
Beforerr <zzj956959688@gmail.com>
2025-06-02 21:20:08 -0700

    feat: Julia's `get_experiments`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MadrigalWeb
 using Test
+using TestItems, TestItemRunner
 
-@testset "MadrigalWeb.jl" begin
+@testitem "MadrigalWeb.jl" begin
     # Write your tests here.
+    @test_nowarn get_experiments("http://millstonehill.haystack.mit.edu", 30, 1998, 1, 19, 0, 0, 0, 1998, 12, 31, 23, 59, 59) == []
 end
